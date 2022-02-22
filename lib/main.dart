8bd3db3e48889352d16518c9cb5f78f44c085bfe
AMAN2001SAHU <aman.sahu3010@gmail.com>
2022-02-22 21:51:28 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tut_android_studios/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,13 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home : Material (
-          child : Center(
-            child: Container(
-              child: Text("30 Day Flutter Tutorial"),
-            ),
-          ),
-        )
+        home : Homepage(),
     );
   }
 }
