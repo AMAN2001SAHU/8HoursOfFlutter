1c4321123d6ffbd7946df1267f04fbbc99140e27
AMAN2001SAHU <aman.sahu3010@gmail.com>
2022-03-01 19:45:00 +0530

    Day 8 Again Completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:tut_android_studios/Pages/Screens/home_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tut_android_studios/utils/routes.dart';
+import 'package:tut_android_studios/widgets/theme.dart';
 import 'Pages/Screens/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -16,22 +15,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        // home : Homepage(),
+      // home : Homepage(),
       themeMode: ThemeMode.light,
-      theme : ThemeData(primarySwatch: Colors.blue,
-        fontFamily: GoogleFonts.lato().fontFamily,
-        // primaryTextTheme: GoogleFonts.latoTextTheme(),
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.light
-            // primarySwatch: Colors.green
-      ),
-      // debugShowCheckedModeBanner: false,
-      initialRoute: "/",
+      theme: MyTheme.lightTheme(context),
+      darkTheme: ThemeData(brightness: Brightness.light
+          // primarySwatch: Colors.green
+          ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: MyRoutes.homeRoute,
       routes: {
-          "/" : (context) => LoginPage(),
-        MyRoutes.homeRoute : (context) => Homepage(),
-        MyRoutes.loginRoute : (context) => LoginPage(),
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => Homepage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
